c2e46515290396ad01d64cc3fdf22613b94b4972
Sadat <85846183+Sadat-09@users.noreply.github.com>
2024-09-19 00:21:38 +0600

    Add files via upload

---
+++ Array/sortcolors.cpp
@@ -0,0 +1,72 @@
+
+#include <bits/stdc++.h>
+#define IO                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+typedef long long int ll;
+typedef long double LD;
+#define w(t)     \
+    int t;       \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+using namespace std;
+bool check_prime(int n) {
+  bool is_prime = true;
+
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1) {
+    is_prime = false;
+  }
+
+  for (int i = 2; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      is_prime = false;
+      break;
+    }
+  }
+
+  return is_prime;
+}
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Implementing Bubble Sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    IO;
+    solve();
+    // w(t);
+    return 0;
+}
